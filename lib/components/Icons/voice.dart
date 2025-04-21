28e8dcbf2026ef54805909798a6699b0f3428e59
gitmeas02 <measreaksmey02@gmail.com>
2025-04-22 00:25:35 +0700

    'm'\

--- lib/components/Icons/voice.dart
+++ lib/components/Icons/voice.dart
@@ -1,18 +1,25 @@
-
 import 'package:flutter/material.dart';
 
-class Voice extends StatefulWidget{
+class Voice extends StatefulWidget {
   const Voice({super.key});
   @override
-  State<Voice> createState()=> _Voice();
+  State<Voice> createState() => _Voice();
 }
-class _Voice extends State<Voice>{
 
+class _Voice extends State<Voice> {
   @override
   Widget build(BuildContext context) {
-    return IconButton(onPressed: (){},
-    iconSize: 72,
-    icon: const Icon(Icons.favorite),
+    return IconButton(
+      onPressed: () {
+        // Add functionality here
+      },
+      iconSize: 24, // Adjusted size for better visibility
+      icon: const Icon(
+        Icons.mic, // Changed to a microphone icon for relevance
+        color: Colors.blue, // Added color for better appearance
+      ),
+      padding: const EdgeInsets.all(12.0), // Adjusted padding for balance
+      tooltip: 'Voice Input', // Added tooltip for accessibility
     );
   }
 }
